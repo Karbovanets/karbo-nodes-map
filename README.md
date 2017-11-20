efa395f60e79900bd00c9de5a70d4cc7b07f23a8
sheppardv <vivchar.danylo@gmail.com>
2017-11-20 16:28:07 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Karbo nodes map
+
+Visualizes KRB nodes on the map.
+
+## How to run
+`npm i`
+`node app.js`
+Open localhost:8080.
+
+It's caching nodes' ips every 24 hrs into `.cache` folder.
---
+++ app.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const locations = require('./locations');
+
+locations.cacheLocations();
+setInterval(locations.cacheLocations, 36e5); // 1hr
+
+app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+
+// blog home page
+app.get('/', async (req, res) => {
+    res.render('home', { locations: await locations.getCachedLocations() });
+});
+
+app.listen(8080);
+
+console.log('listening on port 8080');
