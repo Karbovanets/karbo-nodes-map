ad540c461b709078459804a8cdf4c2d3976c5c85
Max Bondar <4bondar@gmail.com>
2018-08-02 12:29:36 +0300

    fix-cache-algo
    fix cache algo
    add cmd arguments

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const locations = require("./getallpeers.js");
 
+const argv = require("yargs")
+  .default("port", 32348)
+  .default("freegeoserverUrl", "127.0.0.1:8080")
+  .usage("Usage: $0 --startnode=[str] --interval=[num] [[ --port=[num]], --freegeoserverUrl=[str]]")
+    .help('h')
+    .alias('h', 'help').argv;
+let   interval =3600;//sec
 
-app.set('view engine', 'ejs');
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + '/vendor'));
 // blog home page
@@ -16,11 +22,22 @@ app.get('/locations', async (req, res) => {
 });
 app.get('/getmastenodes', async (req, res) => {
 
-    res.send(await locations.getMasterNodes());
+    res.send(await locations.getMasterNodes(argv.freegeoserverUrl));
 });
+if (!argv.startnode) {
+    console.log("set --startnode=<node>");
+    return;
+}
+if (!argv.interval) {
+    console.log("interval set to default: " + interval);
+    
+} else {
+    if (argv.interval > interval) interval = argv.interval;
+}
 
-locations.getLocations();
+locations.cacheLocations(argv.startnode, argv.port, argv.freegeoserverUrl);
 
 app.listen(8081);
-
+console.log("interval set to : " + interval+" s.");
+setInterval(locations.cacheLocations, interval * 1000, argv.startnode, argv.port);
 console.log('listening on port 8081');
